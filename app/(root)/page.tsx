375ab9ca0c33f7c2038277abb657ab10f831d6cd
Insu Hong <hinsu93@naver.com>
2023-07-29 13:20:39 +0900

    shadcn-button 추가

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
-      <p>Hello Admin DashBoard</p>
+      <div className="p-4">
+        <Button size="default" variant="destructive">
+          Click me
+        </Button>
+      </div>
     </>
   );
 }
