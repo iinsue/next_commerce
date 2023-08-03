42674e19bbe2f03afb81f26e74938cf2dcf57ed0
Insu Hong <hinsu93@naver.com>
2023-08-03 18:21:24 +0900

    shadcn-ui command&combo box 추가

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
 
+import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+
 const Navbar = () => {
   return (
     <div className="border-b">
       <div className="flex items-center h-16 px-4">
-        <div>This will be a store switcher</div>
-        <div>This will be the routes</div>
+        <StoreSwitcher />
+        <MainNav className="mx-6" />
         <div className="flex items-center ml-auto space-x-4">
           <UserButton afterSignOutUrl="/" />
         </div>
