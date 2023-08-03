92eb501d9aa85600b99eb5779394a962ae5c4bf6
Insu Hong <hinsu93@naver.com>
2023-08-03 18:32:00 +0900

    store-switcher type 추가

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,3 +1,35 @@
-export default function StoreSwitcher() {
+"use client";
+
+import { Store } from "@prisma/client";
+
+import { PopoverTrigger } from "@/components/ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useParams, useRouter } from "next/navigation";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+  typeof PopoverTrigger
+>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  items: Store[];
+}
+
+export default function StoreSwitcher({
+  className,
+  items = [],
+}: StoreSwitcherProps) {
+  const storeModal = useStoreModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const formattedItems = items.map((item) => ({
+    label: item.name,
+    vlaue: item.id,
+  }));
+
+  const currentStore = formattedItems.find(
+    (item) => item.vlaue === params.storeId
+  );
+
   return <div>Store Switcher</div>;
 }
