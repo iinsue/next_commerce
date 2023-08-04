a3107961f02d823fa7d15b2264bc00d6c9caed04
Insu Hong <hinsu93@naver.com>
2023-08-04 18:14:53 +0900

    store select 생성

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,19 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany();
+
   return (
     <div className="border-b">
       <div className="flex items-center h-16 px-4">
--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,10 +1,27 @@
 "use client";
 
 import { Store } from "@prisma/client";
-
-import { PopoverTrigger } from "@/components/ui/popover";
-import { useStoreModal } from "@/hooks/use-store-modal";
+import { useState } from "react";
 import { useParams, useRouter } from "next/navigation";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Button } from "@/components/ui/button";
+import { Check, ChevronsUpDown, PlusCircle, StoreIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<
   typeof PopoverTrigger
@@ -24,12 +41,74 @@ export default function StoreSwitcher({
 
   const formattedItems = items.map((item) => ({
     label: item.name,
-    vlaue: item.id,
+    value: item.id,
   }));
 
   const currentStore = formattedItems.find(
-    (item) => item.vlaue === params.storeId
+    (item) => item.value === params.storeId
   );
 
-  return <div>Store Switcher</div>;
+  const [open, setOpen] = useState(false);
+
+  const onStoreSelect = (store: { value: string; label: string }) => {
+    setOpen(false);
+    router.push(`/${store.value}`);
+  };
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          role="combobox"
+          aria-expanded={open}
+          aria-label="Select a store"
+          className={cn(`w-[200px] justify-between`, className)}
+        >
+          <StoreIcon className="w-4 h-4 mr-2" />
+          {currentStore?.label}
+          <ChevronsUpDown className="w-4 h-4 ml-auto opacity-50 shrink-0" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="Search store..." />
+            <CommandEmpty>No store found.</CommandEmpty>
+            <CommandGroup heading="Stores">
+              {formattedItems.map((store) => (
+                <CommandItem key={store.value} className="text-sm">
+                  <StoreIcon className="w-4 h-4 mr-2" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator />
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+                onSelect={() => {
+                  setOpen(false);
+                  storeModal.onOpen();
+                }}
+              >
+                <PlusCircle className="w-5 h-5 mr-2" />
+                Create Store
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
 }
